bd6f84a5f4e88e3da8156883b28e69845082a47c
Rainer Müller <rainer@websimple.be>
2013-05-30 14:38:58 +0200

    import depuis la fabrique

---
+++ lang/paquet-shop_livraison_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'shop_livraison_description' => 'Gérer les livraison avec Shop',
+	'shop_livraison_nom' => 'Shop Livraisons',
+	'shop_livraison_slogan' => 'Gérer les livraison avec Shop',
+);
+
+?>
---
+++ shop_livraison_pipelines.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Utilisations de pipelines par Shop Livraisons
+ *
+ * @plugin     Shop Livraisons
+ * @copyright  2013
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Shop_livraison\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+	
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
+
+
+
+?>
