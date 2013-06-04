45b8d3a94b93d3a5d17e8297d88d9a94e9e513ac
Rainer Müller <rainer@websimple.be>
2013-06-04 15:08:35 +0200

    configuration et les noms de unités

--- lang/livraison_fr.php
+++ lang/livraison_fr.php
@@ -12,6 +12,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_exemple' => 'Exemple',
 	'cfg_exemple_explication' => 'Explication de cet exemple',
 	'cfg_titre_parametrages' => 'Paramétrages',
+	
+    // L
+    'label_unite_defaut'=>'L\'unité par défaut',    
+    'label_unite_rien'=>'Acune unité',
+    'label_unite_cl'=>'Cl',        
+    'label_unite_cm3'=>'cm3',     
+    'label_unite_g'=>'Grammes', 
+    'label_unite_kg'=>'Kg', 
+    'label_unite_l'=>'Litres',      
+    'label_unite_m3'=>'m3',            
 
 	// T
 	'titre_livraison' => 'Livraisons',
