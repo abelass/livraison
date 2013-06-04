fa2b81429d71c008777b2bc5dd06e2b576beca17
Rainer Müller <rainer@websimple.be>
2013-06-04 14:51:41 +0200

    plus besoin

--- shop_livraison_pipelines.php
+++ shop_livraison_pipelines.php
@@ -13,35 +13,5 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 	
 
 
-function shop_livraison_formulaire_charger($flux){
-    $form=$flux['args']['form'];
-    if($form == 'configurer_shop_livraison'){
- //Intervention sur la config 
-    include_spip('inc/config');
-    if(!$cfg['zones_livraison']=_request('zones_livraison')){
-        $zones_livraison=lire_config('shop_livraison/zones_livraison',array());
-            $flux['data']['zones_livraison']='';
-            foreach ($zones_livraison as $key => $value) {
-                if($key)$flux['data']['zones_livraison'].="$key,$value\n";
-            }
-        }
-    }
-    return $flux;    
-}
-
-function shop_livraison_formulaire_traiter($flux){
-    // intervenir sur la configuration du plugin
-    $form=$flux['args']['form'];
-    if($form == 'configurer_shop_livraison'){
-        include_spip('inc/config');
-        include_spip('inc/shop_livraison');
-        $cfg=lire_config('shop_livraison');
-        $cfg['zones_livraison']  = zones2array(_request('type_liens'));
-        
-
-    ecrire_config('shop_livraison',$cfg);
-    }
-    return $flux;
-}
 
 ?>
