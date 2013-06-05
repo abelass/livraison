a66159019b6df77ed3541ee9e247b3c229b6c1a5
Rainer Müller <rainer@websimple.be>
2013-06-05 15:54:37 +0200

    langues

--- lang/livraison_fr.php
+++ lang/livraison_fr.php
@@ -13,8 +13,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_exemple_explication' => 'Explication de cet exemple',
 	'cfg_titre_parametrages' => 'Paramétrages',
 	
+    //E
+    'explication_montant_defaut'=>'Montant s\'appliquant aux pays non attachés à une zone de livraison',
+
 
     // L
+    'label_montant_defaut'=>'Montant par défaut',       
     'label_unite_defaut'=>'L\'unité par défaut',    
     'label_unite_rien'=>'Acune unité',
     'label_unite_cl'=>'Cl',        
@@ -31,7 +35,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
      
 
 	// T
+	'texte_montant_defaut_non_definit'=>'Aucun Montant par défaut n\'est définit, les pays non attachés à une zone seront exemptés de frais de livraison. Dans Configuration vous pouvez definir un montant pas défaut',
 	'titre_ajouter_pays_continent' => 'Ajouter les pays de ce continent à cette zone',	
+	
 	'titre_livraison' => 'Livraison',
 	'titre_livraisons' => 'Livraisons',	
 	'titre_page_configurer_shop_livraison' => 'Shop Livraisons',	
