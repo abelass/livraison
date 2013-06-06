8565a576f0e891d18d858dae18803aa1632bf163
Rainer Müller <rainer@websimple.be>
2013-06-06 16:13:35 +0200

    bon nom du fichier

--- lang/paquet-shop_livraisons_fr.php
+++ lang/paquet-shop_livraisons_fr.php
@@ -6,9 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'shop_livraison_description' => 'Gérer les livraison avec Shop',
-	'shop_livraison_nom' => 'Shop Livraisons',
-	'shop_livraison_slogan' => 'Gérer les livraison avec Shop',
+	'shop_livraisons_description' => 'Gérer les livraison avec Shop',
+	'shop_livraisons_nom' => 'Shop Livraisons',
+	'shop_livraisons_slogan' => 'Gérer les livraison avec Shop',
 );
 
 ?>
